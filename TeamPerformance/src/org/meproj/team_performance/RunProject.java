d33a52f57db2fa88e0a794f8777e3847cf984725
rkmalaiya <ritesh.opensource@gmail.com>
2014-03-09 14:45:25 +0530

    Random Commits
    Signed-off-by: rkmalaiya <ritesh.opensource@gmail.com>

--- TeamPerformance/src/org/meproj/team_performance/RunProject.java
+++ TeamPerformance/src/org/meproj/team_performance/RunProject.java
@@ -18,14 +18,14 @@ public class RunProject {
 		
 		Vector vector = new Vector();
 		
-//		try {
-//			//config.startMR("svn_jan2014", "svn_jan2014_mr_output", Formats.SVN_LOG);
-//			//config.startMR("email_jan2014", "email_jan2014_mr_output", Formats.MAIL_CSV);
-//			
-//		} catch (ClassNotFoundException | IOException | InterruptedException e) {
-//			
-//			e.printStackTrace();
-//		}
+		try {
+			//config.startMR("svn_jan2014", "svn_jan2014_mr_output", Formats.SVN_LOG);
+			config.startMR("email_jan2014", "email_jan2014_mr_output", Formats.MAIL_CSV);
+			
+		} catch (ClassNotFoundException | IOException | InterruptedException e) {
+			
+			e.printStackTrace();
+		}
 //		
 //		try {
 //			vector.createAndWrite("svn_jan2014_mr_output/part-r-00000", "hadoop-mapreduce-project", "svn_jan2014_vector/svn_jan2014_vector_file.txt");
@@ -33,22 +33,22 @@ public class RunProject {
 //			// TODO Auto-generated catch block
 //			e.printStackTrace();
 //		}
-		
-		try {
-			KMeansCluster cluster = new KMeansCluster();
-			cluster.startClustering("svn_jan2014_vector", "svn_jan2014_canopy", "svn_jan2014_kmeansCluster/");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
+//		
+//		try {
+//			KMeansCluster cluster = new KMeansCluster();
+//			cluster.startClustering("svn_jan2014_vector", "svn_jan2014_canopy", "svn_jan2014_kmeansCluster");
+//		} catch (ClassNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+				
 //		WordCount count = new WordCount();
 //		try {
 //			count.main1(new String[]{"svn_jan2014_mr_output", "svn_jan2014_mr_output_count"});
