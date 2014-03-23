49a219113caf4ae2d7cb461fe1f0bc4f3e883e39
rkmalaiya <ritesh.opensource@gmail.com>
2014-03-23 14:38:50 +0530

    Commit with proper vector for all
    Signed-off-by: rkmalaiya <ritesh.opensource@gmail.com>

--- TeamPerformance/src/org/meproj/team_performance/RunProject.java
+++ TeamPerformance/src/org/meproj/team_performance/RunProject.java
@@ -2,6 +2,7 @@ package org.meproj.team_performance;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.StringTokenizer;
 
 import org.hadoop.mr.CombileAll;
 import org.hadoop.mr.MRConfig;
@@ -9,6 +10,7 @@ import org.hadoop.mr.WordCount;
 import org.hadoop.mr.clustering.KMeansCluster;
 import org.hadoop.utils.format.FormatHandler.Formats;
 import org.hadoop.vector.Vector;
+import org.hadoop.vector.VectorForIndividuals;
 import org.meproj.team_performance.xmlParser.XmlParser;
 
 public class RunProject {
@@ -17,51 +19,61 @@ public class RunProject {
 		
 		MRConfig config = new MRConfig();
 		
+//		try {
+//			config.startMR("svn_jan2014", "svn_jan2014_mr_output", Formats.SVN_LOG);
+//			config.startMR("email_jan2014", "email_jan2014_mr_output", Formats.MAIL_CSV);
+//			config.startMR("issues_jan2014_csv", "issues_jan2014_csv_mr_output", Formats.ISSUE_LOG);	
+//			
+//		} catch (ClassNotFoundException | IOException | InterruptedException e) {
+//			
+//			e.printStackTrace();
+//		}
+//		
+//		// *********************************** Merge all files as per attributes for vector ************ 
+//		
+//		
+//		new CombileAll().configNStart();
+//		
+//		// *********************************** Create Vectors *****************************
+//		
 		
+		VectorForIndividuals vector = new VectorForIndividuals();		
 		
 		try {
-			config.startMR("svn_jan2014", "svn_jan2014_mr_output", Formats.SVN_LOG);
-			config.startMR("email_jan2014", "email_jan2014_mr_output", Formats.MAIL_CSV);
-			config.startMR("issues_jan2014_csv", "issues_jan2014_csv_mr_output", Formats.ISSUE_LOG);	
+			//vector.createAndWrite("svn_jan2014_mr_output/part-r-00000", "hadoop-mapreduce-project", "svn_jan2014_vector/svn_jan2014_vector_file.txt");
+			//vector.createAndWrite("email_jan2014_mr_output/part-r-00000", "hadoop-mapreduce-project", "email_jan2014_vector/email_jan2014_vector_file.txt");
 			
-		} catch (ClassNotFoundException | IOException | InterruptedException e) {
+			String allProjects = "hadoop-common-project,hadoop-hdfs-project,hadoop-mapreduce-project,hadoop-yarn-project,pig,hive,hbase";
+			StringTokenizer tokenizer = new StringTokenizer(allProjects, ",");
+			String projecName;
 			
+			while (tokenizer.hasMoreTokens()) {
+				projecName = tokenizer.nextToken();
+				vector.createAndWrite("all_jan2014_mr_output/part-r-00000",projecName , "vectors/"+projecName+"_vector_file.txt");
+			}
+			
+		} catch (IOException | ParseException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		// *********************************** Merge all files as per attributes for vector ************ 
-		
-		
-		new CombileAll().configNStart();
-		
-		// *********************************** Create list of common Names *****************************
-		
-		
-		
-		
-//		Vector vector = new Vector();		
-//		try {
-//			//vector.createAndWrite("svn_jan2014_mr_output/part-r-00000", "hadoop-mapreduce-project", "svn_jan2014_vector/svn_jan2014_vector_file.txt");
-//			vector.createAndWrite("email_jan2014_mr_output/part-r-00000", "hadoop-mapreduce-project", "email_jan2014_vector/email_jan2014_vector_file.txt");
-//		} catch (IOException | ParseException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-////		
-//		try {
-//			KMeansCluster cluster = new KMeansCluster();
-//			//cluster.startClustering("svn_jan2014_vector", "svn_jan2014_canopy", "svn_jan2014_kmeansCluster");
-//			cluster.startClustering("email_jan2014_vector", "email_jan2014_canopy", "email_jan2014_kmeansCluster");
-//		} catch (ClassNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		try {
+			KMeansCluster cluster = new KMeansCluster();
+			//cluster.startClustering("svn_jan2014_vector", "svn_jan2014_canopy", "svn_jan2014_kmeansCluster");
+			//cluster.startClustering("email_jan2014_vector", "email_jan2014_canopy", "email_jan2014_kmeansCluster");
+			
+			cluster.startClustering("vectors", "vectors_group_canopy", "vectors_group_kmeansCluster");
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 				
 //		WordCount count = new WordCount();
